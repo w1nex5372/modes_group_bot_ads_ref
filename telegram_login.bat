8a2d7a349c1305180b4371965bbbb2bafad27524
Lukas <105519908+w1nex5372@users.noreply.github.com>
2026-09-16 22:28:44 +0300

    Expose Telegram login resend command in helper

--- telegram_login.bat
+++ telegram_login.bat
@@ -3,6 +3,7 @@ cd /d "%~dp0"
 if "%~1"=="" (
   echo Naudojimas:
   echo   telegram_login.bat send +3706XXXXXXX
+  echo   telegram_login.bat resend
   echo   telegram_login.bat finish KODAS
   echo   telegram_login.bat finish KODAS TAVO_2FA_PASSWORD
   pause
